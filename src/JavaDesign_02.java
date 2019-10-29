130c259a747a2a5f27e9a01037c242fdb7b246ff
ApplePieme <55231505+ApplePieme@users.noreply.github.com>
2019-10-29 12:11:48 +0800

    整理布局

--- src/JavaDesign_02.java
+++ src/JavaDesign_02.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class JavaDesign_02{
+public class JavaDesign_02 {
     /*    private double x=0;
         private double y=0;
         private int r=0;
@@ -49,17 +49,17 @@ public class JavaDesign_02{
         }*/
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        JFrame f = new JFrame();
+        /*JFrame f = new JFrame();
         Circle circle = new Circle();
-        circle.setSize(100,100);
+        circle.setSize(100, 100);
         f.setVisible(true);
         f.setSize(800, 800);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLocationRelativeTo(null);
         Container c = f.getContentPane();
         c.setLayout(new GridLayout(7, 6));
-        JTextField[] t = { new JTextField("小球半径", 8), new JTextField(15), new JTextField("小球初速度", 8),
-                new JTextField(15), new JTextField("小球加速度", 8), new JTextField(15) };
+        JTextField[] t = {new JTextField("小球半径", 8), new JTextField(15), new JTextField("小球初速度", 8),
+                new JTextField(15), new JTextField("小球加速度", 8), new JTextField(15)};
         t[0].setEditable(false);
         t[2].setEditable(false);
         t[4].setEditable(false);
@@ -68,7 +68,32 @@ public class JavaDesign_02{
         }
         c.add(circle);
         Thread th = new Thread(circle);
-        th.start();
+        th.start();*/
+        JFrame f = new JFrame("小球运动程序");
+        f.setVisible(true);
+        f.setSize(800, 800);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLocationRelativeTo(null);
+        Container container = f.getContentPane();
+        container.setLayout(new FlowLayout());
+        JPanel[] panels = new JPanel[2];
+        panels[0] = new JPanel();
+        panels[0].setLayout(new GridLayout(3, 6));
+        panels[1] = new JPanel();
+        container.add(panels[0]);
+        container.add(panels[1]);
+        JTextField[] t = {new JTextField("小球半径", 8), new JTextField(15), new JTextField("小球初速度", 8),
+                new JTextField(15), new JTextField("小球加速度", 8), new JTextField(15)};
+        t[0].setEditable(false);
+        t[2].setEditable(false);
+        t[4].setEditable(false);
+        for (int i = 0; i < 6; i++) {
+            panels[0].add(t[i]);
+        }
+        Circle circle = new Circle();
+        panels[1].add(circle);
+        Thread thread = new Thread(circle);
+        thread.start();
     }
 }
 
@@ -76,14 +101,14 @@ class Circle extends JPanel implements Runnable {
 
     private double x = 0;
     private double y = 0;
-    private int r = 10;
+    private int r = 80;
     double degree = Math.PI / 3;
-    double speed=1;
-    boolean right=true;
+    double speed = 1;
+    boolean right = true;
 
     public void paint(Graphics g) {
         super.paint(g);
-        g.drawOval((int)x,(int) y, r, r);
+        g.drawOval((int) x, (int) y, r, r);
         //x += 10;
         //r += 1;
         /*x = x + speed * Math.cos(degree);
@@ -93,15 +118,16 @@ class Circle extends JPanel implements Runnable {
         if (x > 10000+r || x < r)
             degree = Math.PI - degree;*/
 
-        if(right)
-            x+=10;
+        if (right)
+            x += 10;
         else
-            x-=10;
-        if(x>800-2*r)
-            right=false;
-        if(x<r)
-            right=true;
+            x -= 10;
+        if (x > 800 - 2 * r)
+            right = false;
+        if (x < r)
+            right = true;
     }
+
     public void run() {
         // TODO Auto-generated method stub
         while (true) {
